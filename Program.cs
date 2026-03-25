257e44cc8c9976e95d45d48605da84fa2c55b765
Mariam Abdelrahman Youssef <156664021+Mariam0-0@users.noreply.github.com>
2026-03-25 21:31:31 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,152 @@
+﻿using System.ComponentModel;
+using System.Numerics;
+
+namespace G_NET_9_AdvC_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Question 01
+            //==================================================
+            //Q1: What is a generic class? Why use generics?
+            //==================================================
+
+            #endregion
+
+            #region Question 02
+            //==================================================
+            //Q2: Write a generic class Container<T> with Add and Get methods.
+            //==================================================
+
+            #endregion
+
+            #region Question 03
+            //==================================================
+            //Q3:What are multiple type parameters? Write Pair<TKey, TValue>.
+            //==================================================
+
+            #endregion
+
+            #region Question 04
+            //==================================================
+            //Q4: What is a generic method? Write Swap<T> method.
+            //==================================================
+
+            #endregion
+
+            #region Question 05
+            //==================================================
+            //Q5: Write a generic method FindMax<T> that finds maximum value
+            //==================================================
+
+            #endregion
+
+            #region Question 06
+            //==================================================
+            //Q6: What is a generic interface? Write IRepository<T>.
+            //==================================================
+
+            #endregion
+
+            #region Question 07
+            //==================================================
+            //Q7: What is the 'struct' constraint? Write an example.
+            //==================================================
+
+            #endregion
+
+            #region Question 08
+            //==================================================
+            //Q8: What is the 'class' constraint? Write an example.
+            //==================================================
+
+            #endregion
+
+            #region Question 09
+            //==================================================
+            //Q9: What is the 'new()' constraint? Write an example.
+            //==================================================
+
+            #endregion
+
+            #region Question 10
+            //==================================================
+            //Q10:  What is the interface constraint? Write an example.
+            //==================================================
+
+            #endregion
+
+            #region Question 11
+            //==================================================
+            //Q11: What is the base class constraint? Write an example.
+            //==================================================
+
+            #endregion
+
+            #region Question 12
+            //==================================================
+            //Q12: How do you apply multiple constraints? Write an example.
+            //==================================================
+
+            #endregion
+
+            #region Question 13
+            //==================================================
+            //Q13: What does the 'default' keyword do in generics?
+            //==================================================
+
+            #endregion
+
+            #region Question 14
+            //==================================================
+            //Q14: Write a SafeList<T> that returns default when the index is invalid.
+            //==================================================
+
+            #endregion
+
+            #region Question 15
+            //==================================================
+            //Q15: What is covariance? Explain the 'out' keyword.
+            //==================================================
+
+            #endregion
+
+            #region Question 16
+            //==================================================
+            //Q16: What is contravariance? Explain the 'in' keyword.
+            //==================================================
+
+            #endregion
+
+            #region Question 17
+            //==================================================
+            //Q17: What is the difference between covariance and contravariance?
+            //==================================================
+
+            #endregion
+
+            #region Question 18
+            //==================================================
+            //Q18: How do static members work in generic types?
+            //==================================================
+
+            #endregion
+
+            #region Question 19
+            //==================================================
+            //Q19: How can you inherit from a generic class?
+            //==================================================
+
+            #endregion
+
+            #region Question 20
+            //==================================================
+            //Q20: Complete Exercise - Create a generic Cache<TKey, TValue>with Add, Get, Remove, Contains, and expiration support.
+            //==================================================
+
+            #endregion
+
+        }
+    }
+}
